5cdc96bf6fb74466675ccc57047705bd4f1fde28
kondetiparabrahmam <134903475+kondetiparabrahmam@users.noreply.github.com>
2025-01-22 15:37:33 +0530

    Update Part2.py

--- Part2.py
+++ Part2.py
@@ -1,6 +1,5 @@
 #aws lambda code for 
-#How to Make a Chatbot Using Amazon Lex and AWS Lambda (Python) | Conversational AI Part 2
-# https://youtu.be/W6T-RFei6SY
+#How to Make a Chatbot Using Amazon Lex and AWS Lambda (Python) | Conversational AI 
 import json
 import datetime
 import time
